36ecff1c03b63f75c94acdfb9009331a8aed132a
bildan <bildanjauhary@gmail.com>
2026-06-11 21:46:26 +0700

    feat: add list of message from submission form loacal storage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,9 +4,12 @@ export default function About () {
     return (
         <div className="flex flex-col items-center w-full h-[100vh]">
         <Navbar/>
-        <div className="flex justify-center items-center w-full flex-col h-[100vh]">
-            <h1 className="text-4xl">Hello</h1>
-            <p>You are at the about page.</p>
+        <div className="flex w-[70%] items-center flex-col h-[100vh]">
+            <div className="flex flex-col w-[100%] mt-24">
+                <h1 className="text-[6rem] ">Hello</h1>
+                <h4 className="mb-4 text-xl text-white mt-12">You are at the about page.</h4>
+                <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nostrum facilis debitis ad. Vitae, rerum! Dolore quo provident excepturi iure architecto voluptas officia, at commodi necessitatibus deleniti fuga ipsum corporis alias modi dignissimos perferendis fugit odit numquam unde illo nulla maiores totam dolor illum. Impedit eligendi perferendis quam unde magnam aliquam rem necessitatibus, aliquid molestias fuga molestiae. Temporibus, nisi non. Ab quae suscipit ducimus magnam quo mollitia tempora officia dolorum quidem expedita. Voluptates, quos aspernatur veniam suscipit minus iusto voluptas ipsa fugit, sit tempore cumque! Sed praesentium officiis ducimus adipisci? Quibusdam cumque explicabo assumenda delectus dolor, incidunt iste ipsum autem? Dolorem corporis dolores amet quas, excepturi dolor at accusantium repellendus expedita, quasi iste magni sequi cum obcaecati enim neque illo id corrupti accusamus? Amet, qui! Suscipit totam iure ex, dicta dignissimos beatae incidunt sequi magni tempora omnis cumque placeat quisquam laboriosam nisi fuga esse fugiat reiciendis perferendis animi, maxime quod enim, recusandae iste ad! Rerum, quae id! Quis reiciendis eaque dolor animi. Sit sapiente, est voluptatibus quidem beatae numquam reiciendis tenetur eos blanditiis, doloribus laboriosam autem maiores a maxime recusandae incidunt dignissimos eveniet odit veritatis dolorem rerum delectus dicta aspernatur velit. Illum, id. Optio saepe quo corrupti impedit velit quas?</p>
+            </div>
         </div>
     </div>
     )
--- src/pages/Contact-us.jsx
+++ src/pages/Contact-us.jsx
@@ -1,37 +1,57 @@
+import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
+
 export default function ContactUs(){
+  const [message, setMessage] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+     function getUserMessage(){
+        const userMessage = JSON.parse(localStorage.getItem("message"))
+        if(userMessage) {
+            setMessage(userMessage)
+            setLoading(false)
+        }
+     }
+    getUserMessage()
+
+  },[setMessage, setLoading])
 
     function handleSubmit(e){
-        e.preventDefault()
-        try{
-            const data = new FormData(e.target)
-            const formatData = Object.fromEntries(data.entries())
-            const dataContact = []
-            const contactMessage = {}
+      e.preventDefault()
+      try{
+        const data = new FormData(e.target)
+        const formatData = Object.fromEntries(data.entries())
+        let dataContact = []
+        const contactMessage = {}
 
-            if(!formatData) throw new Error("Data kosong")
-            for (const props in formatData){
-                if(!(formatData[props])){
-                    throw new Error("Data Kosong")
-                }
-                contactMessage[props] = formatData[props]
+        if(!formatData) throw new Error("Data kosong")
+        for (const props in formatData){
+            if(!(formatData[props])){
+                throw new Error("Data Kosong")
             }
-            console.log(contactMessage)
-            dataContact.push(contactMessage)
-            window.localStorage.setItem("message", JSON.stringify(dataContact))
-        } catch(err){
-            alert(err.message)
+            contactMessage[props] = formatData[props]
         }
+        const userMessage = JSON.parse(localStorage.getItem("message"))
+
+        if(userMessage) dataContact = [...userMessage]
+        dataContact.push(contactMessage)
+        setMessage(dataContact)
+        setLoading(false)
+        localStorage.setItem("message", JSON.stringify(dataContact))
+      } catch(err){
+         alert(err.message)
+      }
     }
 
     return(
-        <div className="flex flex-col items-center w-full h-[100vh]">
+        <div className="flex flex-col items-center w-full h-fit">
             <Navbar/>
-            <div className="w-full h-[100vh] flex flex-col gap-24 items-center">
+            <div className="w-full h-[100vh] flex flex-col items-center ">
                   <p className="text-[12rem] text-white pt-[4rem] font-bold">Contact Us</p>
                   <form action="" 
                   onSubmit={(e) => {handleSubmit(e)}}
-                  className="flex flex-col gap-4 w-[50%] mt-3">
+                  className="flex flex-col gap-4 w-[50%] mt-24">
                     <div className="flex flex-col">
                         <label htmlFor="name">Name</label>
                         <input 
@@ -53,12 +73,33 @@ export default function ContactUs(){
                     </div>
                     <div className="w-full justify-end">
                         <button type="submit" 
-                        className="bg-white w-24 h-10 text-slate-900 font-medium text-sm cursor-pointer">
+                        className="bg-[#6FCF97] w-24 h-10 text-slate-900 font-medium text-sm
+                        active:bg-[#6FCF97] cursor-pointer hover:bg-[#2FA084]">
                             SUBMIT
                         </button>
                     </div>
                   </form>
+                  {
+                     loading ? <></> :
+                     <MessageList message={message}/>
+                  }
             </div>
+        </div>
+    )
+}
+
+function MessageList({message}){
+    return(
+        <div className="w-[50%] h-fit pb-10 mt-6">
+            {message?.map((item, index) => (
+                <div
+                 key={index}
+                 className="mt-12">
+                    <h4 className="text-2xl text-white">{item?.name}</h4>
+                    <p className="text-xs text-[#6FCF97]">{item?.email}</p>
+                    <p className="pt-5">{item?.message}</p>
+                </div>
+            ))}
         </div>
     )
 }
